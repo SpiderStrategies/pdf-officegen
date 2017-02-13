ba634bafb964b7b679cc6420021320f9b240b72e
codecounselor <nate.good@spiderstrategies.com>
2017-02-13 17:32:09 -0500

    fixes #4 - numeric sort

--- test/powerpoint-test.js
+++ test/powerpoint-test.js
@@ -24,3 +24,10 @@ test('Convert options use density=72 if none is set', t => {
   const o = p._getConvertOptions({})
   t.is(o['density'], 72)
 })
+
+test('sort images', t => {
+  // 1-100 to ensure both the doc and page are sorted independently
+  const images = ['img/img-21-9.png', 'img/img-1-1.png', 'img/img-1-100.png']
+  const sorted = p._sortImages(images)
+  t.deepEqual(sorted, ['img/img-1-1.png', 'img/img-1-100.png', 'img/img-21-9.png'])
+})
